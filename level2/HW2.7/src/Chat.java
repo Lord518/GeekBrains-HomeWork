4eef753b0de9951da45cbaa6d8b1751d98cebefa
Stas <stas.chernomorets@icloud.com>
2020-12-14 19:53:06 +0300

    HW2.7

---
+++ level2/HW2.7/src/Chat.java
@@ -0,0 +1,8 @@
+public interface Chat {
+    void broadcastMessage(String message);
+    boolean isNicknameOccupied(String nickname);
+    void subscribe(ClientHandler client);
+    void unsubscribe(ClientHandler client);
+    AuthenticationService getAuthenticationService();
+     boolean privateMessage(String massage, String name);
+}
