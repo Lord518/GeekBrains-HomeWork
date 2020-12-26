bc0985ab39f62f708cd5a3c282e6fdb9d1bf1b71
Stas <stas.chernomorets@icloud.com>
2020-12-26 12:31:06 +0300

    HW3.1

--- level3/HW1/src/Box.java
+++ level3/HW1/src/Box.java
@@ -2,45 +2,46 @@ import java.util.ArrayList;
 
 public class Box<T extends Fruit> {
     private String name;
-    private ArrayList<T> fruit;
+    private ArrayList<T> fruits;
 
     public Box(String name) {
-        this.fruit = new ArrayList<>();
-       this.name = name;
+        this.fruits = new ArrayList<>();
+        this.name = name;
     }
 
     public void addArrayList(T e) {
-        fruit.add(e);
-    }
-    public float getWeight(){
-        T e = fruit.get(0);
-
-        return e.getWeight() * fruit.size();
+        fruits.add(e);
     }
 
-    public void setFruits(ArrayList<T> arr) {
-        this.fruit = arr;
+    public float getWeight() {
+        T e = fruits.get(0);
+
+        return e.getWeight() * fruits.size();
+    }
+
+    public void setFruits(ArrayList<T> fruit) {
+        this.fruits = fruit;
     }
 
     public ArrayList<T> getFruits() {
-        return fruit;
+        return fruits;
     }
 
     public boolean compare(Box a) {
         float currentBox = getWeight();
         float otherBox = a.getWeight();
-        return currentBox==otherBox;
+        return currentBox == otherBox;
     }
 
-    public void moveTo(Box<T> other) {
-        other.setFruits(getFruits());
-         setFruits(new ArrayList<>());
+    public void moveTo(Box<T> b) {
+        b.setFruits(getFruits());
+        setFruits(new ArrayList<>());
     }
 
-   public void print(){
-        System.out.print(name+" ");
-        for (T e : fruit){
-        System.out.print(e.toString()+" ");
+    public void print() {
+        System.out.print(name + " ");
+        for (T e : fruits) {
+            System.out.print(e.toString() + " ");
         }
         System.out.println();
     }
